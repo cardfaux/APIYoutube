2beaf1c5345380076574c3400d2aa3fe57daebb5
James Hagood <cardfaux@tutanota.com>
2020-02-19 11:52:39 -0600

    added express-validator

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('User Route'));
+router.post(
+	'/',
+	[
+		check('name', 'Name Is Required')
+			.not()
+			.isEmpty(),
+		check('email', 'Please Use A Valid E-Mail').isEmail(),
+		check(
+			'password',
+			'Please Enter A Password With 6 or More Characters'
+		).isLength({ min: 6 })
+	],
+	(req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		res.send('User Route');
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// CONNECT TO THE DATABASE
 connectDB();
 
+// MIDDLEWARE FOR BODY PARSER
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API IS RUNNING ON PORT 5000'));
+
+// DEFINE ROUTES
+app.use('/api/users', require('./routes/user'));
 
 const PORT = process.env.PORT || 5000;
 
